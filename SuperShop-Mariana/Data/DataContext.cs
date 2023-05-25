1b4381b29115c282a7dcc84979354753c9019fa4
Bikiway <mariana.95@outlook.pt>
2023-05-25 14:15:03 +0100

    First Video

---
+++ SuperShop-Mariana/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop_Mariana.Data.Entities;
+
+namespace SuperShop_Mariana.Data
+{
+    public class DataContext : DbContext //herda a dele
+    {
+        public DbSet<Products> products { get; set; } //Criar a tabela
+        public DataContext(DbContextOptions<DataContext> options) : base(options) //injetamos o options
+        {
+            
+        }
+    }
+}
---
+++ SuperShop-Mariana/Data/Entities/Products.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuperShop_Mariana.Data.Entities
+{
+    public class Products
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Image")] //Aparecer na página da web. Só image e não ImageUrl.
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Last Purchase")]
+        public DateTime LastPurchase { get; set; }
+
+
+        [Display(Name = "Last Sale")]
+        public DateTime LastSale { get; set; }
+
+
+        [Display(Name="Is Available")]
+        public bool IsAvaiable { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+    }
+}
