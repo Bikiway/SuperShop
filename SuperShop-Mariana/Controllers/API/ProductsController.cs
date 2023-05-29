8c156ca98f7f4f1f181dd545f0406bdbcb97ce26
Bikiway <mariana.95@outlook.pt>
2023-05-29 17:46:43 +0100

    Seventh video: After API

---
+++ SuperShop-Mariana/Controllers/API/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperShop_Mariana.Data;
+
+namespace SuperShop_Mariana.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductsRepository _productsRepository;
+        public ProductsController(IProductsRepository productsRepository)
+        {
+            _productsRepository = productsRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts() 
+        {
+            return Ok(_productsRepository.GetAll()); //Agarra tudo e embrulha no Json.
+        }
+
+    }
+}
